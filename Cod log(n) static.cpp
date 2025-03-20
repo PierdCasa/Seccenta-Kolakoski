afe9f57a79b748f13c87f283fc84f018ec30b4c2
PierdCasa <mariopislariu69@gmail.com>
2025-03-20 16:45:04 +0200

    Create Cod log(n) static.cpp

---
+++ Cod log(n) static.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+
+
+using namespace std;
+
+ofstream g("KolakoskiLogN.out");
+#define NMAX 922337203685477599
+unsigned long long int n;
+
+unsigned long long p[NMAX];
+
+unsigned long long int increment(unsigned long long int k)
+{
+    if(p[k]==0)
+        p[k]=22;
+    if(p[k]==11)
+        {p[k]=1;
+        return 1;}
+    else if(p[k]==22)
+    {
+        p[k]=2;
+        return 2;
+    }
+    else if(p[k]==1)
+    {
+        p[k]=increment(k+1)==1?2:22;
+        return 2;
+    }
+    else if(p[k]==2)
+    {
+        p[k]=increment(k+1)==1?1:11;
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    cin>>n;
+    if(n>=1)
+    g<<1;
+    if(n>=2)
+    g<<2;
+    if(n>=3)
+    {for(unsigned long long int i=0;i<n-2;++i)
+        {g<<increment(0);}
+    }
+    //OBSERVATIE INPUTUL ESTE -3;
+    
+    g.close();
+    /*
+    122112122122112112212112122112112122122112122
+
+    1221121221221121122121121221121121221221121221
+    */
+    return 0;
+}
